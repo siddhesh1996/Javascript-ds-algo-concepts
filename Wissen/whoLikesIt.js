3cc9935ea9544f112b3ceff357a9262a0254ee96
unknown <ksiddhesh96.sk@gmail.com>
2021-05-28 13:09:03 +0530

    who likes it solution codewarss

---
+++ Wissen/whoLikesIt.js
@@ -0,0 +1,28 @@
+function likes(names) {
+    // TODO
+    let result = 'likes this'
+    switch (names.length) {
+        case 0:
+            result = `no one ${result.replace('like','likes')}`
+            break;
+        case 1:
+            result = `${names[0]} ${result.replace('like','likes')}`
+            break;
+        case 2:
+            result = `${names[0]} and ${names[1]} ${result}`
+            break;
+        case 3:
+            result = `${names[0]}, ${names[1]} and ${names[2]} ${result}`
+            break;
+        default:
+            let othersCount = names.length - 2;
+            result = `${names[0]}, ${names[1]} and ${othersCount} others like this`;
+            break;
+    }
+
+    return result;
+}
+
+let names = ['Alex', 'Jacob', 'Mark', 'Max'];
+
+console.log(likes(names));
